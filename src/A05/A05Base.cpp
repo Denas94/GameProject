ceca4f193b8a7d6d2b627b914c8f64b63efda28a
Joan <romani21@hotmail.com>
2017-10-17 15:42:14 +0200

    Pantalla amb text

--- src/A05/A05Base.cpp
+++ src/A05/A05Base.cpp
@@ -1,4 +1,6 @@
 #include <SDL.h>		// Always needs to be included for an SDL app
+#include <SDL_image.h>  //Fons de pantalla-personatge-moviment
+#include <SDL_ttf.h>    //Fonts de text
 
 //Game general information
 #define SCREEN_WIDTH 800
@@ -8,6 +10,7 @@ int main(int, char*[]) {
 
 	// --- INIT ---
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
+	if (TTF_Init() != 0) throw "No es pot inicialitzar SDL_ttf";
 
 	// --- WINDOW ---
 	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
@@ -17,11 +20,34 @@ int main(int, char*[]) {
 	SDL_Renderer *renderer{ SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
 	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
 
-	// --- SPRITES ---
+	// --- SPRITES --- (Textures)
 
-		// --- Animated Sprite ---
+	const Uint8 imgFlags{ IMG_INIT_PNG | IMG_INIT_JPG };
+	if (!(IMG_Init(imgFlags)& imgFlags)) throw "Error: SDL_image init";
+
+		//BG -- (Fons de pantalla)
+	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer,"../../res/img/bg.jpg") };
+	if (bgTexture == nullptr) throw "No s'han pogut crear les textures";
+	SDL_Rect bgRect{ 0,0, SCREEN_WIDTH, SCREEN_HEIGHT };
+
+		//Player
+	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer,"../../res/img/kintoun.png") };
+	if (playerTexture == nullptr) throw "No s'han pogut crear les textures";
+	SDL_Rect playerRect{ 0,0, 350, 189 };
+	SDL_Rect playerTarget{ 0,0,100,100 };
+
+		// --- Animated Sprite --- (DIJOUS)
 
 	// --- TEXT ---
+
+	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf",80) };
+	if (font == nullptr) throw "No es pot inicialitzar the TTF_Font";
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font,"My first SDL game", SDL_Color{255,150,0,255}) };
+	if (tmpSurf == nullptr) throw "Unable to create the SDL text surface";
+	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(renderer,tmpSurf) };
+	SDL_Rect textRect{ 100,50,tmpSurf->w, tmpSurf->h };
+	SDL_FreeSurface(tmpSurf);
+	TTF_CloseFont(font);
 
 	// --- AUDIO ---
 
@@ -34,25 +60,41 @@ int main(int, char*[]) {
 			switch (event.type) {
 			case SDL_QUIT:		isRunning = false; break;
 			case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
+			case SDL_MOUSEMOTION: //playerRect.x = event.motion.x - playerRect.w/2; playerRect.y = event.motion.y - playerRect.h/2;
+				playerTarget.x = event.motion.x - playerRect.w / 2; playerTarget.y = event.motion.y - playerRect.h / 2; break;
 			default:;
 			}
 		}
 
 		// UPDATE
+			//Interpolació vector moviment -- SMOOTH
+		playerRect.x += (playerTarget.x - playerRect.x) / 10;
+		playerRect.y += (playerTarget.y - playerRect.y) / 10;
 
 		// DRAW
-			//Background
 		SDL_RenderClear(renderer);
+			//Background
+		
 			//Animated Sprite
+
+		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
+		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+		SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
 		SDL_RenderPresent(renderer);
 
 	}
 
 	// --- DESTROY ---
+	SDL_DestroyTexture(bgTexture);
+	SDL_DestroyTexture(textTexture);
+	SDL_DestroyTexture(playerTexture);	
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
+
 	// --- QUIT ---
+	IMG_Quit();
+	TTF_Quit();
 	SDL_Quit();
 	return 0;
 }
